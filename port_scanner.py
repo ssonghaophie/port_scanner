879c4285ccdd83684c065e7e82fd05704b209028
sylvia <stan@smith.edu>
2023-05-03 19:54:12 -0400

    check if target ip alive

--- port_scanner.py
+++ port_scanner.py
@@ -55,6 +55,43 @@ def fin_scan(target_IP,port):
         return True
     return False
 
+
+def port_scan(target_IP, port_range):
+    # record start time
+   
+    open_port_list=[]
+    num_close=0
+    for port in port_range:
+        if mode=="normal":
+            banner=norm_scan(target_IP, port)
+            if banner is not None:
+                try:
+                    service = socket.getservbyport(port)
+                except:
+                    service=""
+                open_port_list.append([port,service,banner])
+            else:
+                num_close+=1
+
+        if mode=="syn":
+            if syn_scan(target_IP, port):
+                try:
+                    service = socket.getservbyport(port)
+                except:
+                    service=""
+                open_port_list.append([port,service])
+            else: num_close+=1
+
+        if mode=="fin":
+            if fin_scan(target_IP, port):
+                try:
+                    service = socket.getservbyport(port)
+                except:
+                    service=""
+                open_port_list.append([port,service])
+            else: num_close+=1
+    return open_port_list, num_close
+
 # Create an argument parser for port scanning modes
 parser = argparse.ArgumentParser(description="Different modes of port scanning: Normal Port Scanning, TCP SYN Scanning, TCP FIN Scanning")
 # parser.add_argument('target', metavar='target', type=str, help='Target IP address')
@@ -72,68 +109,43 @@ target_IP =parser.parse_args().target_ip
 # set the port list
 port_range=[]
 if ports=="all":
-    port_range=list(range(1,65536))
+    port_range=list(range(0,65536))
 elif ports=="known":
-    port_range=list(range(1,1024))
+    port_range=list(range(0,30))
 
 if order == "random":
     random.shuffle(list(port_range))
 
+# Send an ICMP echo request packet to the target device
+icmp_request_packet = IP(dst=target_IP)/ICMP(type=8)
+icmp_response_packet = sr1(icmp_request_packet, timeout=2, verbose=False)
 
-# record start time
-start_time= datetime.datetime.now()
-open_port_list=[]
+is_alive=False
+# If the response packet is not None and has an ICMP type of 0, the target is alive
+if icmp_response_packet is not None and icmp_response_packet[ICMP].type == 0:
+    is_alive=True
 
-port_range=range(100)
-num_open=0
-num_close=0
-for port in port_range:
+if is_alive:
+    start_time= datetime.datetime.now()
+    open_port_list, num_close = port_scan(target_IP,port_range)
+    # record end time
+    end_time= datetime.datetime.now()
+
+    # print final results
+    print("Start port scan at: " + str(start_time))
+    print("Interesting ports on " + target_IP)
+    print(f"Not shown:{num_close} closed ports")
     if mode=="normal":
-        banner=norm_scan(target_IP, port)
-        if banner is not None:
-            try:
-                service = socket.getservbyport(port)
-            except:
-                service=""
-            open_port_list.append([port,service,banner])
-        else:
-            num_close+=1
-
-    if mode=="syn":
-        if syn_scan(target_IP, port):
-            try:
-                service = socket.getservbyport(port)
-            except:
-                service=""
-            open_port_list.append([port,service])
-        else: num_close+=1
-
-    if mode=="fin":
-        if fin_scan(target_IP, port):
-            try:
-                service = socket.getservbyport(port)
-            except:
-                service=""
-            open_port_list.append([port,service])
-        else: num_close+=1
-
-# record end time
-end_time= datetime.datetime.now()
-
-# print final results
-print("Start port scan at: " + str(start_time))
-print("Interesting ports on " + target_IP)
-print(f"Not shown:{num_close} closed ports")
-if mode=="normal":
-    print("PORT     STATE     SERVICE       BANNER")
-else:
-    print("PORT     STATE     SERVICE")
-
-for p in open_port_list:
-    if mode=="normal":
-        print(f"{p[0]}/tcp    open    {p[1]}      {p[2]}")
+        print("PORT     STATE     SERVICE       BANNER")
     else:
-        print(f"{p[0]}/tcp    open    {p[1]}")
-print(f"scan done! in {target_IP} ({len(open_port_list)} host up) scanned in {end_time-start_time}secs")
+        print("PORT     STATE     SERVICE")
 
+    for p in open_port_list:
+        if mode=="normal":
+            print(f"{p[0]}/tcp    open    {p[1]}      {p[2]}")
+        else:
+            print(f"{p[0]}/tcp    open    {p[1]}")
+    print(f"scan done! in {target_IP} ({len(open_port_list)} host up) scanned in {end_time-start_time}secs")
+else:
+    print("Target IP is not alive")
 
